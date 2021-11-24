9196b7f2ddaaa5281dfb413e4c698bc0a2375a6e
mtonosaki <manabu@tomarika.com>
2021-11-24 18:31:49 +0900

    1st commit

---
+++ tSecreta/tSecreta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tSecreta
+//
+//  Created by Manabu Tonosaki on 2021/11/24.
+//  MIT License (c)2021 Manabu Tonosaki all rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tSecreta/tSecretaTests/tSecretaTests.swift
@@ -0,0 +1,33 @@
+//
+//  tSecretaTests.swift
+//  tSecretaTests
+//
+//  Created by Manabu Tonosaki on 2021/11/24.
+//
+
+import XCTest
+@testable import tSecreta
+
+class tSecretaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
