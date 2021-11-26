3cc22cf00999f237dc33435f19864d95ed394252
mtonosaki <manabu@tomarika.com>
2021-11-26 14:40:57 +0900

    PoC AzureAD authentication

--- tSecreta/tSecreta/ViewController.swift
+++ tSecreta/tSecreta/ViewController.swift
@@ -6,6 +6,7 @@
 //  MIT License (c)2021 Manabu Tonosaki all rights reserved.
 
 import UIKit
+import MSAL
 
 class ViewController: UIViewController {
 
@@ -13,6 +14,36 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // to open azuread authentication
+        let adconfig = MySecret()
+        let config = MSALPublicClientApplicationConfig(clientId: adconfig.clientId)
+        if let application = try? MSALPublicClientApplication(configuration: config){
+            #if os(iOS)
+                let webviewParameters = MSALWebviewParameters(authPresentationViewController: self)
+            #else
+                let webviewParameters = MSALWebviewParameters()
+            #endif
+            
+            let interactiveParameters = MSALInteractiveTokenParameters(scopes: adconfig.scopes, webviewParameters: webviewParameters)
+            application.acquireToken(with: interactiveParameters, completionBlock: {
+                (result, error) in
+                guard let authResult = result, error == nil else {
+                    print(error!.localizedDescription)
+                    return
+                }
+                // Get access token from result
+                let accessToken = authResult.accessToken
+                
+                // You will want to get the account identifier to retreeve and reuse the account for later acquireToken calls
+                let accountIdentifier = authResult.account.identifier
+            })
+        } else {
+            print("Unable to create applicaiton")
+        }
+                
+        // Attached logo image from AppIcon
+        // TODO: precision version image
         let img = UIImage(named: "AppIcon")
         imageLogo.image = img;
     }
