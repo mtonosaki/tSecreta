b549b4e3c1c12980c3786d53a0c4954a84e82203
mtonosaki <manabu@tomarika.com>
2021-12-02 17:12:15 +0900

    detail view text fiields

--- tSecreta/tSecreta/ViewControllerDetail.swift
+++ tSecreta/tSecreta/ViewControllerDetail.swift
@@ -7,9 +7,43 @@
 
 import UIKit
 
-final class ViewControllerDetail : UIViewController {
+final class ViewControllerDetail : UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var textRubi: UITextField!
+    @IBOutlet weak var textCaption: UITextField!
+    @IBOutlet weak var textAccountId: UITextField!
+    @IBOutlet weak var textPassword: UITextField!
+    @IBOutlet weak var textEmail: UITextField!
+    @IBOutlet weak var textMemo: UITextView!
+    
+    
     public var note: Note? = nil
     
     override func viewDidLoad() {
+        // for auto "Next" at Return key
+        textRubi.delegate = self
+        textCaption.delegate = self
+        textAccountId.delegate = self
+        textPassword.delegate = self
+        textEmail.delegate = self
+        
+        textRubi.text = note?.GetLatest(key: "CaptionRubi")
+        textCaption.text = note?.GetLatest(key: "Caption")
+        textAccountId.text = note?.GetLatest(key: "AccountID")
+        textPassword.text = note?.GetLatest(key: "Password")
+        textEmail.text = note?.GetLatest(key: "Email")
+        textMemo.text = note?.GetLatest(key: "Memo")
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        let nextTag = textField.tag + 1
+        if let nextTextField = self.view.viewWithTag(nextTag) {
+            nextTextField.becomeFirstResponder()
+        }
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        // TODO: Persist edited text
     }
 }
