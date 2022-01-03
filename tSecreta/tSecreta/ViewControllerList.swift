e1ee423fc16647f270b1f25adedfdcde2d1df0fd
mtonosaki <manabu@tomarika.com>
2022-01-03 15:16:25 +0900

    セクション追加

--- tSecreta/tSecreta/ViewControllerList.swift
+++ tSecreta/tSecreta/ViewControllerList.swift
@@ -6,48 +6,63 @@
 //  MIT License (c)2021 Manabu Tonosaki all rights reserved.
 
 import UIKit
+import Tono
 
 final class ViewControllerList : UITableViewController {
     
     public var noteList: NoteList? = nil
     public var userObjectId: String = ""
     private var noteTarget: Array<Note>? = nil
-        
+    private var sectionNotes = Dictionary<String, Array<Note>>()
+    private var sectionOrder = Array<String>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.allowsSelection = true
+        resetList()
     }
     
     func resetList() {
-        if noteTarget == nil {
-            let notes = noteList?.Notes
-            if let notes = notes {
-                noteTarget  = notes.filter{ $0.CheckDeleted() == false }.sorted(by: {
-                    let rubia = ($0.GetLatest(key: "CaptionRubi") ?? $0.GetLatest(key: "Caption") ?? "").trimmingCharacters(in: .whitespacesAndNewlines).lowercased()
-                    let rubib = ($1.GetLatest(key: "CaptionRubi") ?? $1.GetLatest(key: "Caption") ?? "").trimmingCharacters(in: .whitespacesAndNewlines).lowercased()
-                    return rubia < rubib
-                })
-            }
+        let notes = noteList?.Notes
+        if let notes = notes {
+            noteTarget  = notes.filter{ $0.CheckDeleted() == false }.sorted(by: {
+                let rubia = ($0.GetLatest(key: "CaptionRubi") ?? $0.GetLatest(key: "Caption") ?? "").trimmingCharacters(in: .whitespacesAndNewlines).lowercased()
+                let rubib = ($1.GetLatest(key: "CaptionRubi") ?? $1.GetLatest(key: "Caption") ?? "").trimmingCharacters(in: .whitespacesAndNewlines).lowercased()
+                return rubia < rubib
+            })
+            sectionNotes = Dictionary(grouping: noteTarget!, by: { String($0.GetLatest(key: "CaptionRubi")?.first ?? ".") })
+            sectionOrder = sectionNotes.keys.sorted(by: { $0 < $1 })
         }
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionOrder.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionOrder[section]
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        resetList()
-        return noteTarget?.count ?? 0
+        return sectionNotes[sectionOrder[section]]?.count ?? 0
+    }
+    
+    private func getNote(at indexPath: IndexPath) -> Note {
+        guard let notes = sectionNotes[sectionOrder[indexPath.section]] else {
+            fatalError()
+        }
+        let note = notes[indexPath.row]
+        return note
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell", for: indexPath)
-        resetList()
-        let note = noteTarget?[indexPath.row]
-        if let note = note {
-            let caption = note.GetLatest(key: "Caption") ?? ""
-            cell.textLabel?.text = caption
-            cell.detailTextLabel?.text = note.GetLatest(key: "AccountID")
-            cell.imageView?.image = UIImage(named: findBrandLogo(name: caption))
-            cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
-        }
+        let note = getNote(at: indexPath)
+        let caption = note.GetLatest(key: "Caption") ?? ""
+        cell.textLabel?.text = caption
+        cell.detailTextLabel?.text = note.GetLatest(key: "AccountID")
+        cell.imageView?.image = UIImage(named: findBrandLogo(name: caption))
+        cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
         return cell
     }
     
@@ -58,10 +73,7 @@ final class ViewControllerList : UITableViewController {
             title:  "ID",
             handler: {
                 (action: UIContextualAction, view: UIView, success :(Bool) -> Void) in
-                guard let note = self.noteTarget?[indexPath.row] else {
-                    success(false)
-                    return
-                }
+                let note = self.getNote(at: indexPath)
                 if let id = note.GetLatest(key: "AccountID") {
                     UIPasteboard.general.string = id
                     showToast(message: "Copy account ID", color: UIColor.systemGreen, view: self.parent?.view ?? self.view)
@@ -79,10 +91,7 @@ final class ViewControllerList : UITableViewController {
             title:  "PW",
             handler: {
                 (action: UIContextualAction, view: UIView, success :(Bool) -> Void) in
-                guard let note = self.noteTarget?[indexPath.row] else {
-                    success(false)
-                    return
-                }
+                let note = self.getNote(at: indexPath)
                 if let pw = note.GetLatest(key: "Password") {
                     UIPasteboard.general.string = pw
                     showToast(message: "! Copy Password !", color: UIColor.systemOrange, view: self.parent?.view ?? self.view)
@@ -95,26 +104,18 @@ final class ViewControllerList : UITableViewController {
         //actionPw.image = UIImage(named: "swipePw")
         actionPw.backgroundColor = .systemOrange
         
-        let actionDelete = UIContextualAction(
-            style: .normal,
-            title: "Delete",
-            handler: {
-                (action: UIContextualAction, view: UIView, success :(Bool) -> Void) in
-                //                let note = self.noteTarget?[indexPath.row]
-                //                guard let note = note else {
-                //                    success(false)
-                //                    return
-                //                }
-                //                note.SetDeletedFlag(true)
-                //                tableView.deleteRows(at: [indexPath], with: .automatic )
-                //                self.resetList()
-                success(true)
-            }
-        )
-        //actionDelete.image = UIImage(named: "trash")
-        actionDelete.backgroundColor = .systemRed
+//        let actionDelete = UIContextualAction(
+//            style: .normal,
+//            title: "Delete",
+//            handler: {
+//                (action: UIContextualAction, view: UIView, success :(Bool) -> Void) in
+//                success(true)
+//            }
+//        )
+//        //actionDelete.image = UIImage(named: "trash")
+//        actionDelete.backgroundColor = .systemRed
         
-        return UISwipeActionsConfiguration(actions: [actionDelete, actionPw, actionId])
+        return UISwipeActionsConfiguration(actions: [actionPw, actionId])
         
     }
     
@@ -124,10 +125,7 @@ final class ViewControllerList : UITableViewController {
                 guard let destination = segue.destination as? ViewControllerDetail else {
                     fatalError("\(segue.destination)")
                 }
-                guard let note = self.noteTarget?[indexPath.row] else {
-                    return
-                }
-                destination.note = note
+                destination.note = getNote(at: indexPath)
             }
         }
     }
@@ -139,7 +137,6 @@ final class ViewControllerList : UITableViewController {
     @IBAction func didTapCloudSync(_ sender: Any) {
         
         do {
-            
             let encoder = JSONEncoder()
             encoder.dateEncodingStrategy = .formatted(.iso8601PlusMilliSecondsJst)
             let jsonPlaneData = try encoder.encode(noteList!)
